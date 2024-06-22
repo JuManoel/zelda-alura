8f38c28d030a2eb85c5857baab7903f4fa54a8ae
Juan Manoel <gmjm2004@gmail.com>
2024-06-22 16:59:39 -0300

    first commit

---
+++ src/main/java/edu/alura/tlozelda/TlozeldaApplication.java
@@ -0,0 +1,13 @@
+package edu.alura.tlozelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlozeldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlozeldaApplication.class, args);
+	}
+
+}
